eb50378d14025f42b15f3dc26cdb576f48c5435d
Lema kefyalew <lmmbiz2002@gmail.com>
2022-04-20 17:19:07 +0300

    Edited header

--- main.h
+++ main.h
@@ -1,25 +1,15 @@
-#ifndef MAIN_H_
-#define MAIN_H_
+  int print_hex_base(va_list arg, char _case);
+  int print_hex(va_list arg);
+  int print_HEX(va_list arg);
+  int printva_list arg);
+  void print_binary(unsigned int n, unsigned int *printed);
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
+  /**
+   *  * struct specifiersStruct - specifier definition
+   *   * @specifiers: types
+   *    * @{
+                char *specifiers;
+                        int (*printer)(va_list);
+} specifiersStruct;
 
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_char(va_list arg);
-int print_int(va_list arg);
-int print_str(va_list arg);
-int print_unsigned(va_list arg);
-int print_percent(void);
-int print_unsigned_binary(va_list arg);
-int print_oct(va_list arg);
-int print_unsignedIntToHex(unsigned int num, char _case);
-int print_hex_base(va_list arg, char _case);
-int print_hex(va_list arg);
-int print_HEX(va_list arg);
-int print_STR(va_list arg);
-void print_binary(unsigned int n, unsigned int *printed);
-main.h[+] [unix] (17:05 20/04/2022)                                     1,16 Top
--- INSERT --
-
+#endif
