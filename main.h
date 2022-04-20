c197952769e19ed14524f7c4a9a08f9d5451206f
Lema kefyalew <lmmbiz2002@gmail.com>
2022-04-20 17:21:05 +0300

    edited header file

--- main.h
+++ main.h
@@ -1,15 +1,35 @@
-  int print_hex_base(va_list arg, char _case);
-  int print_hex(va_list arg);
-  int print_HEX(va_list arg);
-  int printva_list arg);
-  void print_binary(unsigned int n, unsigned int *printed);
+#ifndef MAIN_H_
+#define MAIN_H_
 
-  /**
-   *  * struct specifiersStruct - specifier definition
-   *   * @specifiers: types
-   *    * @{
-                char *specifiers;
-                        int (*printer)(va_list);
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list arg);
+int print_int(va_list arg);
+int print_str(va_list arg);
+int print_unsigned(va_list arg);
+int print_percent(void);
+int print_unsigned_binary(va_list arg);
+int print_oct(va_list arg);
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_STR(va_list arg);
+void print_binary(unsigned int n, unsigned int *printed);
+
+/**
+ * struct specifiersStruct - specifier definition
+ * @specifiers: types
+ * @printer: printer
+ */
+typedef struct specifiersStruct
+{
+	char *specifiers;
+	int (*printer)(va_list);
 } specifiersStruct;
 
 #endif
